fd223b50e5db8547b7b4c79c81decd4595d923f9
khy236 <khy236@nyu.edu>
2025-01-04 21:14:02 -0800

    Revert "add 3d"
    This reverts commit 712735d7de0270e0a5674af9318fb83980ab5e5d.

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ var bounds = [-75.90202, 38.82094, -71.24568, 42.07133]
 // initialize basemap
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/standard',
+  style: 'mapbox://styles/mapbox/light-v11',
   center: [-73.93366, 40.67941], // Jackson Heights
   pitch: 40,
   bounds: [-73.97374, 40.65206, -73.89378, 40.71295]
@@ -51,40 +51,6 @@ map.on('load', function () {
   })
 
   map.addLayer({
-    'id': '3d-buildings',
-    'source': 'composite',
-    'source-layer': 'building',
-    'filter': ['==', 'extrude', 'true'],
-    'type': 'fill-extrusion',
-    'minzoom': 15,
-    'paint': {
-        'fill-extrusion-color': '#aaa',
-
-        // use an 'interpolate' expression to add a smooth transition effect to the
-        // buildings as the user zooms in
-        'fill-extrusion-height': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'height']
-        ],
-        'fill-extrusion-base': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'min_height']
-        ],
-        'fill-extrusion-opacity': 0.6
-    }
-  })
-
-map.addLayer({
     id: 'line-select-ntas',
     type: 'line',
     source: 'select_ntas_geojson',
